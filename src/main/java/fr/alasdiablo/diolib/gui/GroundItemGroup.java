4a940bb08991b0c541b419aa85f134639951e254
AlasDiablo <25723276+alasdiablo@users.noreply.github.com>
2021-03-08 13:28:16 +0100

    Fix creative tab
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

--- src/main/java/fr/alasdiablo/diolib/gui/GroundItemGroup.java
+++ src/main/java/fr/alasdiablo/diolib/gui/GroundItemGroup.java
@@ -12,24 +12,24 @@ public abstract class GroundItemGroup extends ItemGroup {
     public GroundItemGroup(String label) {
         super(label);
         if (DateRange.IS_WINTER) {
-            super.setBackgroundImage(new ResourceLocation(DiaboloLib.MOD_ID, "ground_winter.png"));
+            super.setBackgroundImage(new ResourceLocation(DiaboloLib.MOD_ID, "textures/gui/container/creative_inventory/ground_winter.png"));
         } else {
-            super.setBackgroundImage(new ResourceLocation(DiaboloLib.MOD_ID, "ground.png"));
+            super.setBackgroundImage(new ResourceLocation(DiaboloLib.MOD_ID, "textures/gui/container/creative_inventory/ground.png"));
         }
         if (DateRange.IS_APRIL_FIRST)
-            super.setBackgroundImage(new ResourceLocation(DiaboloLib.MOD_ID, "ground_april.png"));
+            super.setBackgroundImage(new ResourceLocation(DiaboloLib.MOD_ID, "textures/gui/container/creative_inventory/ground_april.png"));
     }
 
     @Deprecated
     @Override
     public ItemGroup setBackgroundImageName(String texture) {
-        DiaboloLib.logger.debug("setBackgroundImageName in OreItemGroup do nothing, please don't user it.");
+        DiaboloLib.logger.warn("setBackgroundImageName in OreItemGroup do nothing, please don't user it.");
         return this;
     }
 
     @Override
     public ItemGroup setBackgroundImage(ResourceLocation texture) {
-        DiaboloLib.logger.debug("setBackgroundImageName in OreItemGroup do nothing, please don't user it.");
+        DiaboloLib.logger.warn("setBackgroundImageName in OreItemGroup do nothing, please don't user it.");
         return this;
     }
 
